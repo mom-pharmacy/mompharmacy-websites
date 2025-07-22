286f7117e4f808a993f1a3daa4cf50b1950951cd
nizampet-rohith <rohith.nizam123@gmail.com>
2025-07-22 17:09:36 +0530

    joblanding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './pages/Home'
-
+import JobDetails from './pages/Careers/JobDetails'
 function App() {
  
 
   return (
     <Routes>
       <Route path='/' Component={Home} />
+      <Route path='/jobdetails' Component={JobDetails} />
     </Routes>
   )
 }
---
+++ src/pages/Careers/JobDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import JobLanding from '../../components/Careers/JobDetails/JobLanding'
+export default function JobDetails() {
+  return (
+    <JobLanding />
+  )
+}
