7ba164944968e72647568cc481d78f4443b2c8fa
adarsh745 <adarshjanjirala745@gmail.in>
2025-07-23 16:49:20 +0530

    added departments n ourTeam page

--- src/components/OurTeam/Departments.jsx
+++ src/components/OurTeam/Departments.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import CardComp from './CardComp'
+import React from "react";
+import CardComp from "./CardComp";
 
 export default function Departments() {
   return (
     <div>
-      <div className="h-screen flex items-center justify-center flex-col">
-            <h2 className='text-[#00A79B] text-5xl font-bold'> Departments </h2>
-            <p className='text-2xl'>Departments across</p>
-      
-            <div className="flex p-8 gap-10">
-             <CardComp name="Developers" />
-             <CardComp name="Designers" />
-             <CardComp name="Testers" />
-            </div>
-          </div>
-      
+      <div className="flex flex-col items-center">
+        <h2 className="text-[#00A79B] text-5xl font-bold"> founders</h2>
+        <p className="text-2xl">meet the people behind the promise</p>
+      </div>
+
+      <div className="flex overflow-x-auto gap-4 py-4 px-2 justify-center">
+        <CardComp name="Developer"></CardComp>
+
+        <CardComp name="Disigner"></CardComp>
+
+        <CardComp name="Tester"></CardComp>
+
+        <CardComp name="Product Manager"></CardComp>
+
+        <CardComp name="HR"></CardComp>
+
+        <CardComp name="Developer"></CardComp>
+
+        <CardComp name="Developer"></CardComp>
+      </div>
     </div>
-  )
+  );
 }
