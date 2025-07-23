c14717a011bc29cabab675faae9a8a13190cdedb
akhiladharavath <106368196+akhiladharavath@users.noreply.github.com>
2025-07-23 14:03:41 +0530

    added the contactus and career page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './pages/Home'
+import ContactUs from './pages/ContactUs'
 import JobDetails from './pages/Careers/JobDetails'
-
 
 function App() {
  
@@ -10,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' Component={Home} />
+      <Route path='/contactus' Component={ContactUs} />
       <Route path='/jobdetails' Component={JobDetails} />
     </Routes>
   )
