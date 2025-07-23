0aa414ccaf165e640666b7e7cc4b44cea8b59943
pushpa <pushpasenorita@gmail.com>
2025-07-23 18:45:30 +0530

    faqs , ask mom , footer

---
+++ src/components/Home/Faqs.jsx
@@ -0,0 +1,62 @@
+import { q } from "framer-motion/client"
+import { useState } from "react"
+
+const faqsList = [
+    {
+        id: 1,
+        question: "what is mom pharmacy?",
+        answer: "Mom Pharmacy stands for Medicine on Minutes a fast, reliable, and user-friendly platform that delivers essential medicines and healthcare products within just 10 minutes."
+    },
+    {
+        id: 2,
+        question: "what is mom pharmacy?",
+        answer: "Mom Pharmacy stands for Medicine on Minutes  a fast, reliable, and user-friendly platform that delivers essential medicines and healthcare products within just 10 minutes."
+    },
+    {
+        id: 3,
+        question: "what is mom pharmacy?",
+        answer: "Mom Pharmacy stands for Medicine on Minutes  a fast, reliable, and user-friendly platform that delivers essential medicines and healthcare products within just 10 minutes."
+    },
+    {
+        id: 4,
+        question: "what is mom pharmacy?",
+        answer: "Mom Pharmacy stands for Medicine on Minutes  a fast, reliable, and user-friendly platform that delivers essential medicines and healthcare products within just 10 minutes."
+    },
+]
+
+function Faqs() {
+    //state 
+    const [activeAnswer, setActiveAnswer] = useState(null)
+
+    console.log(activeAnswer)
+
+    return <div className="w-[80%] m-auto">
+        <div className="flex items-center justify-center mb-[-20px]">
+            <div className="font-bold text-[50px] text-[#00A79B]">
+                frequently asked questions?
+            </div>
+            <div className="animate-bounce">
+                <img src="/Home/pillmanFaqs.jpg" alt="pillman" />
+            </div>
+        </div>
+        {faqsList.map(item => <div key={item.id} className="cursor-pointer py-4 px-6 border-1 border-[#00A79B] shadow-2xs shadow-[#00A79B] mt-5 mb-5 rounded-xl">
+            <div className="flex justify-between items-center">
+                <button onClick={() => {
+                    console.log("this is logging")
+                    setActiveAnswer(item.id)
+                }}
+                    className="text-[24px] font-medium  cursor-pointer"
+                >{item.question} </button>
+                <span className="cursor-pointer  " onClick={() => {
+                    console.log("this is logging")
+                    setActiveAnswer(item.id)
+                }}>✚</span>
+            </div>
+            {activeAnswer === item.id && <div className=" text-[18px] font-normal">
+                <p className="pt-2">{item.answer}</p>
+            </div>}
+        </div>)}
+    </div>
+}
+
+export default Faqs
