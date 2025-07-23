118e0aa5510a8eedb241186d5f9bed36ff4959a4
mompharmacy <ps42shivakumar@gmail.com>
2025-07-23 11:34:31 +0530

    vision and mission code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './pages/Home'
+import Mission from './components/AboutUs/Mission'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' Component={Home} />
+      <Route path='/aboutus' Component={Mission} />
     </Routes>
   )
 }
