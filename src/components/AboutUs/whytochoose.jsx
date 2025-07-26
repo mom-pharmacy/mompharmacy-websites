d2a93af6cf170dbac04445428942021bbd3f4aab
teja464 <tnukavarapu@gmail.com>
2025-07-26 14:15:57 +0530

    whytochooseus ui commit

--- src/components/AboutUs/whytochoose.jsx
+++ src/components/AboutUs/whytochoose.jsx
@@ -3,11 +3,16 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { motion } from 'framer-motion';
-import easyLogin from '/src/assets/AboutUs/whytochoose/easy_login.png';
-import prescriptionOrder from '/src/assets/AboutUs/whytochoose/prescription_orser.png';
-import quickDelivery from '/src/assets/AboutUs/whytochoose/quick_delivery.png';
-import trustedCare from '/src/assets/AboutUs/whytochoose/trusted_care.png';
-import bloodDonation from '/src/assets/AboutUs/whytochoose/blood_donation.png';
+import easy1 from '../../assets/AboutUs/whytochoose/easy1.png'
+import easy2 from '../../assets/AboutUs/whytochoose/easy2.png'
+import arrow1 from '../../assets/AboutUs/whytochoose/arrow1.png'
+import arrow2 from '../../assets/AboutUs/whytochoose/arrow2.png'
+import prescription1 from '../../assets/AboutUs/whytochoose/pres1.png'
+import prescription2 from '../../assets/AboutUs/whytochoose/pres2.png'
+import quick1 from '../../assets/AboutUs/whytochoose/quick1.png'
+import quick2 from '../../assets/AboutUs/whytochoose/quick2.png'
+import blood1 from '../../assets/AboutUs/whytochoose/blood1.png'
+import blood2 from '../../assets/AboutUs/whytochoose/blood2.png'
 import '@fontsource/fredoka-one';
 import '@fontsource/dm-sans';
 
@@ -15,32 +20,52 @@ const tabs = [
   {
     key: 'easy-login',
     label: 'easy login',
-    text: 'sign in effortlessly with a smooth and secure login process — no fuss, just access in seconds',
-    image: easyLogin,
+    text1: 'just enter your mobile number to proceed',
+    text2:'enter otp sent to your mobile number for quick otp verification',
+    image1: easy1,
+    image2:easy2,
+    image3:arrow1,
+    image4:arrow2
   },
   {
     key: 'prescription-order',
     label: 'prescription order',
-    text: 'just snap and upload your prescription — we’ll take care of the rest, from verification to delivery',
-    image: prescriptionOrder,
+    text1: 'quickly order medicines via prescription ordering',
+    text2:"just upload your prescription from any given options",
+    image1:prescription1,
+    image2:prescription2,
+    image3:arrow1,
+    image4:arrow2
   },
   {
     key: 'quick-delivery',
     label: 'quick delivery',
-    text: 'get your medicines at lightning speed - delivered to your door in just 10 minutes, because your health can’t wait',
-    image: quickDelivery,
+    text1: 'track your order via map view',
+    text2:"order delivered, now rate the delivery & give feedback ",
+    image1:quick1,
+    image2:quick2,
+    image3:arrow1,
+    image4:arrow2
   },
   {
     key: 'trusted-care',
     label: 'trusted care',
-    text: 'from reliable medicines to expert support, we ensure you get the care you deserve',
-    image: trustedCare,
+    text1: 'from reliable medicines to expert support, we ensure you get the care you deserve',
+    text2:"from reliable medicines to expert support, we ensure you get the care you deserve",
+    image1: easy1,
+    image2:easy2,
+    image3:arrow1,
+    image4:arrow2
   },
   {
     key: 'blood-donation',
     label: 'blood donation',
-    text: 'need or want to donate blood? Our platform links patients with nearby donors in real-time — life-saving help at your fingertips',
-    image: bloodDonation,
+    text1: 'register here to become one ofblood donorsat our portal',
+    text2:"search here we will match with nearest blood donor available",
+    image1:blood1,
+    image2:blood2,
+    image3:arrow1,
+    image4:arrow2
   },
 ];
 
@@ -101,12 +126,40 @@ export default function WhyChooseUsBanner() {
         <Slider {...settings} ref={sliderRef}>
           {tabs.map((tab) => (
             <div key={tab.key}>
-              <div className="flex flex-col md:flex-row items-center justify-center gap-6 bg-[#d5ece9] rounded-lg border border-teal-500 mx-4 sm:mx-6 md:mx-10 lg:mx-16 py-6 px-4 sm:py-8 sm:px-6">
-                <div className="text-gray-600 font-['DM_Sans'] text-base sm:text-xl md:text-2xl lg:text-2xl max-w-lg text-center md:text-left">
-                  {tab.text}
+              
+              <div className="flex flex-col md:flex-row items-center justify-center gap-6 bg-[#ffff] rounded-lg border border-teal-500 mx-4 sm:mx-6 md:mx-10 lg:mx-16 py-6 px-4 sm:py-8 sm:px-6">
+                
+                <img
+                  src={tab.image1}
+                  alt={tab.label}
+                  className="w-full sm:w-64 md:w-72 lg:w-80 h-auto rounded-lg shadow-md"
+                />
+                <div className='inline mb-20'>
+                <div className="text-200 font-['DM_Sans'] text-base ">
+                  {tab.text1}
                 </div>
                 <img
-                  src={tab.image}
+                  src={tab.image3}
+                  alt={tab.label}
+                  className="h-20 w-20 rounded-lg shadow-md"
+                />
+                </div>
+                {/* <div className="text-gray-600 font-['DM_Sans'] text-base sm:text-xl md:text-2xl lg:text-2xl max-w-lg text-center md:text-left">
+                  {tab.text}
+                </div> */}
+                <div className='inline mt-30'>
+                
+                <img
+                  src={tab.image4}
+                  alt={tab.label}
+                  className="h-20 w-20 ml-30 rounded-lg shadow-md"
+                />
+                <div className="text-200 font-['DM_Sans'] text-base">
+                  {tab.text2}
+                </div>
+                </div>
+                <img
+                  src={tab.image2}
                   alt={tab.label}
                   className="w-full sm:w-64 md:w-72 lg:w-80 h-auto rounded-lg shadow-md"
                 />
