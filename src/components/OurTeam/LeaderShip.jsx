c294b23ed3ef4447ce645f72a27ce13a43665f7f
SupriyaKoduru4 <kodurusupriya4@gmail.com>
2025-08-04 18:40:57 +0530

    added responsive view and some changes

--- src/components/OurTeam/LeaderShip.jsx
+++ src/components/OurTeam/LeaderShip.jsx
@@ -5,21 +5,21 @@ import ImageComp from "./ImageComp";
 
 export default function EntreprenuorComp(propes) {
   return (
-    <div className=" h-auto m-auto xl:w-[90%] items-center ">
+    <div className=" h-auto m-auto md:p-5 xl:w-[90%] items-center ">
       <div>
         <h1 className="m-10 text-5xl text-[#00A79B] font-semibold text-center ">
           Transforming Healthcare Policy <br/> Through leadership
         </h1>
       </div>
-      <div className=" lg:h-full w-full rounded-2xl border-2 border-[#00A79B] md:grid grid-cols-3 items-center ">
-         <div className="m-auto">
+      <div className=" lg:h-full w-full rounded-2xl md:p-10 border-2 border-[#00A79B] md:grid grid-cols-3 items-center ">
+         <div className="m-auto p-5">
           <img
             src="/pardhu.png"
             className="rounded-3xl border-10 border-[#E6F6F5] "
           ></img>
         </div>
         <div className="col-span-2">
-          <p className="m-3">
+          <p className="m-3 p-3 text-xl ">
             {" "}
             
             To become the most trusted global name in doorstep ealthcare,
