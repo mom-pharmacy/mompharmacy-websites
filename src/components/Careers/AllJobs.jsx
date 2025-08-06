65beb5ee675a839a7d6d390aef6eff410f8e016a
SupriyaKoduru4 <kodurusupriya4@gmail.com>
2025-08-06 09:57:48 +0530

    added footer code and responsive view

--- src/components/Careers/AllJobs.jsx
+++ src/components/Careers/AllJobs.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import JobCard from "../JobCard";
 import { useCareer } from "../../context/career";
 import { useNavigate } from "react-router";
+import Image from '../../assets/Careerpage/backwardarrow.png'
+import Image2 from '../../assets/Careerpage/forwardarrow.png'
 
 const AllJobs = ({ Result, loading, error }) => {
   const { career } = useCareer();
   const navigate = useNavigate();
 
   const jobs = Result === null ? career : Result;
+  const isFlatList = Array.isArray(Result);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const jobsPerPage = 3;
 
   const groupJobsByDepartment = (jobArray) => {
     const grouped = {};
@@ -20,13 +25,26 @@ const AllJobs = ({ Result, loading, error }) => {
     return grouped;
   };
 
-
-  if (loading) return <p className="text-center text-lg">Loading jobs...</p>;
+  if (loading) return <p className="text-center text-sm">Loading jobs...</p>;
   if (error) return <p className="text-center text-red-500">{error}</p>;
 
-  const isFlatList = Array.isArray(Result);
+  let flatJobs = [];
+  if (isFlatList) {
+    flatJobs = jobs;
+  } else if (Array.isArray(jobs)) {
+    jobs.forEach((dept) => {
+      if (Array.isArray(dept.jobUpload)) {
+        flatJobs = flatJobs.concat(dept.jobUpload);
+      }
+    });
+  }
+  const totalPages = Math.ceil(flatJobs.length / jobsPerPage);
+  const startIndex = (currentPage - 1) * jobsPerPage;
+  const currentJobs = flatJobs.slice(startIndex, startIndex + jobsPerPage);
 
-  const groupedJobs = isFlatList ? groupJobsByDepartment(jobs) : null;
+  const handlePrevious = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
+  const handleNext = () =>
+    setCurrentPage((prev) => Math.min(prev + 1, totalPages));
 
   return (
     <div className="px-4 sm:px-5 md:px-10 lg:px-15">
@@ -44,22 +62,12 @@ const AllJobs = ({ Result, loading, error }) => {
       </div>
 
       <div className="mt-8 sm:mt-10 sm:mx-2 mb-10">
-        {!jobs || (Array.isArray(jobs) && jobs.length === 0) ? (
+        {!jobs || flatJobs.length === 0 ? (
           <p className="text-xl text-center text-gray-500">No jobs available</p>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:grid-rows-2 gap-8">
-            {(() => {
-              let flatJobs = [];
-              if (isFlatList) {
-                flatJobs = jobs;
-              } else if (Array.isArray(jobs)) {
-                jobs.forEach((dept) => {
-                  if (Array.isArray(dept.jobUpload)) {
-                    flatJobs = flatJobs.concat(dept.jobUpload);
-                  }
-                });
-              }
-              return flatJobs.slice(0, 8).map((job, idx) => (
+          <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+              {currentJobs.map((job, idx) => (
                 <button
                   onClick={() => navigate("/jobdetails", { state: job })}
                   key={idx}
@@ -72,9 +80,44 @@ const AllJobs = ({ Result, loading, error }) => {
                     vacancies={job.vacancy}
                   />
                 </button>
-              ));
-            })()}
-          </div>
+              ))}
+            </div>
+
+
+            <div className="flex justify-center items-center mt-10 gap-4 flex-col sm:flex-row sm:6">
+              <span className="text-lg font-medium mr-290 sm:tetx-base md:text-lg sm:text-left">
+                Page {currentPage} of {totalPages}
+              </span>
+
+              <button
+                onClick={handlePrevious}
+                disabled={currentPage === 1}
+                className={`px-2 py-3 sm:p-1 bg-[#00a99d] rounded-full ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+                  }`}
+              >
+                <img
+                  src={Image}
+                  alt="backarrow"
+                  className="w-5 sm:w-6"
+                />
+              </button>
+
+              <button
+                onClick={handleNext}
+                disabled={currentPage === totalPages}
+                className={`px-2 py-3 sm:p-1 bg-[#00a99d] rounded-full ${currentPage === totalPages
+                    ? "opacity-50 cursor-not-allowed"
+                    : ""
+                  }`}
+              >
+                <img
+                src={Image2}
+                alt="forwardarrow"
+                className="w-5 sm:w-6"
+                />
+              </button>
+            </div>
+          </>
         )}
       </div>
     </div>
--- src/components/momp.jsx
+++ src/components/momp.jsx
@@ -17,26 +17,26 @@ export default function JobLanding() {
 const[isHover, setHover]=useState(false)
 
   return (
-    <div className="bg-[#00a79b] h-full w-full inline-block pb-20 ">
-      <motion.div className="px-2 ,py-2 flex justify-between">
-        <div className="py-5 px-10">
+    <div className="bg-[#00a79b] h-full w-full inline-block pb-20 min-h-screen overflow-hidden">
+      <motion.div className="px-1, py-1 flex justify-between flex-col md:flex-row items-center">
+        <div className="py-5 md:px-10">
           <Button title="back" link="#" />
         </div>
 
         <motion.div initial={{y:"-20%"}} animate={{y:0}} transition={{duration:1}} className="flex" >
-          <p className="notranslate font-family:Fredoka text-xl rounded-bl-2xl rounded-br-2xl px-10 mb-6 py-3 bg-white inline">
+          <p className=" sm:text-base md:text-lg notranslate font-family:Fredoka text-xl rounded-bl-2xl rounded-br-2xl px-10 mb-6 py-3 bg-white inline">
             be among first few users to try our app and gives us your valuable
             feedback &nbsp;
             <span className="text-[#00a79b] text-xl inline">try now!</span>
           </p>
         </motion.div>
-        <div className="px-10 py-5">
+        <div className="md:px-10 py-5">
           <button className="bg-white rounded-4xl px-5 py-1 h-9 flex justify-center">
             english
           </button>
         </div>
       </motion.div>
-      <div className="flex justify-between ">
+      <div className="flex flex-col lg:flex-row justify-between items-center px-4 lg:px-20 mt-10 gap-10">
         <motion.div
           initial={{
             x: -100,
@@ -51,24 +51,24 @@ const[isHover, setHover]=useState(false)
             delay: 0.1,
           }}
         >
-          <div className="flex py-30">
+          <div className="flex py-30 sm:flex-row items-center">
             <div>
               <img src={logo} alt="logo" className="w-45 h-45" />
             </div>
             <div>
-              <p className="text-7xl text-white font-['Fredoka_One'] py-10 px-5">
+              <p className="text-7xl sm:text-5xl lg:text-7xl text-white font-['Fredoka_One'] py-10 px-5">
                 pharmacy
               </p>
             </div>
           </div>
-          <div className="flex justify-between bg-white h-14 w-150 relative bottom-25 left-25 px-5 py-1 rounded-xl">
+          <div className="flex justify-between bg-white h-14 w-150 relative bottom-25 left-25 px-5 py-1 rounded-xl lg:justify-start max-w-full">
             <img
               src={Heart}
               alt="heart"
               className="w-13 h-15 relative bottom-7"
             />
 
-            <p className="text-3xl font-['Fredoka_One'] mb-2">
+            <p className="text-3xl font-['Fredoka_One'] mb-2 sm:text-lg md:text-2xl">
               medicine & more on minutes
             </p>
             <img src={Clock} alt="heart" className="w-12 h-12 relative top-5" />
@@ -77,12 +77,12 @@ const[isHover, setHover]=useState(false)
         <motion.div
           onHoverStart={() => setHover(true)}
           onHoverEnd={() => setHover(false)}
-          className="px-20 py-15"
+          className=" relative max-w-xl px-20 py-15"
         >
           <motion.img
             src={img6}
             alt="icon"
-            className="absolute z-0 h-30 top-21 right-100"
+            className="absolute  md:block z-0 h-30 top-5 right-100"
             animate={
               isHover ? { x: 5, y: -5, rotate: 20 } : { x: 0, y: 0, rotate: 0 }
             }
@@ -93,7 +93,7 @@ const[isHover, setHover]=useState(false)
             alt=""
             animate={isHover ? { x: 0, y: 5 } : { x: 0, y: 0 }}
             transition={{ type: "spring", stiffness: 100 }}
-            className="absolute h-18 right-2 z-1"
+            className="absolute md:block  h-18 right-2 z-1"
           />
           <motion.img
             src={img2}
@@ -114,14 +114,14 @@ const[isHover, setHover]=useState(false)
             alt="prescription"
             animate={isHover ? { x: 5, y: -5 } : { x: 0, y: 0 }}
             transition={{ type: "spring", stiffness: 100 }}
-            className="absolute z-1 h-30 top-120 left-195"
+            className="absolute z-1 h-20 top-110 left-8"
           />
           <motion.img
             src={img5}
             alt="map"
             animate={isHover ? { x: 5, y: -5 } : { x: 0, y: 0 }}
             transition={{ type: "spring", stiffness: 100 }}
-            className="absolute z-1 h-30 top-135 right-8"
+            className="absolute z-1 h-30 top-120 right-8"
           />
           <motion.img
             src={rectangle1}
@@ -137,169 +137,4 @@ const[isHover, setHover]=useState(false)
 
   );
 }
-
-
-
-
-
-// import React from "react";
-// import { motion } from "framer-motion";
-// import { Link } from "react-router-dom";
-// import Button from "./Buttons/BorderButton";
-// import logo from '../../public/Home/logo.jpg';
-// import Heart from '../assets/momp/dotheart.png';
-// import Clock from '../assets/momp/clock.png'
-// import rectangle1 from '../assets/momp/mainImg.png'
-// import img1 from '../assets/momp/img1.png'
-// import img2 from '../assets/momp/bloodDonor.png'
-// import img3 from '../assets/momp/location.png'
-// import img4 from '../assets/momp/prescription.png'
-// import img5 from '../assets/momp/map.png'
-// import img6 from '../assets/momp/icon.png'
-// export default function JobLanding() {
-//   // Define variants for the parent container (rectangle1)
-//   const parentVariants = {
-//     initial: { scale: 1 }, // Initial state for parent
-//     hover: {
-//       scale: 1.05, // Slight scale up on hover
-//       transition: {
-//         duration: 0.3,
-//         staggerChildren: 0.1, // Stagger child animations by 0.1 seconds
-//       },
-//     },
-//   };
-
-//   // Define variants for the child images
-//   const childVariants = {
-//     initial: {
-//       x: 0,
-//       y: 0,
-//     //   opacity: 0.8, // Initial opacity for child images
-//     },
-//     hover: (i) => ({
-//       // Using a custom prop 'i' (index) for varied movement
-//       x: i % 2 === 0 ? -20 : 20, // Move left or right depending on the index
-//       y: i % 2 === 0 ? 20 : -20, // Move up or down depending on the index
-//       opacity: 1, // Full opacity on hover
-//       transition: {
-//         duration: 0.3,
-//         ease: "easeOut", // Smooth transition
-//       },
-//     }),
-//   };
-
-//   return (
-//     <div className="bg-[#00a79b] h-full w-full inline-block pb-20">
-//       <motion.div className="px-2 ,py-2 flex justify-between">
-//         <div className="py-5 px-10">
-//           <Button title="back" link="#" />
-//         </div>
-
-//         <div className="flex">
-//           <p className="font-family:Fredoka text-xl rounded-bl-2xl rounded-br-2xl px-10 mb-2 py-5 bg-white inline">
-//             be among first few users to try our app and gives us your valuable
-//             feedback &nbsp;
-//             <span className="text-[#00a79b] text-xl inline">try now!</span>
-//           </p>
-//         </div>
-//         <div className="px-10 py-5">
-//           <button className="bg-white rounded-4xl px-5 py-1 h-9 flex justify-center">
-//             english
-//           </button>
-//         </div>
-//       </motion.div>
-//       <div className="flex justify-between ">
-//         <motion.div
-//           initial={{
-//             x: -100,
-//             opacity: 0,
-//           }}
-//           animate={{
-//             x: 50,
-//             opacity: 1,
-//           }}
-//           transition={{
-//             duration: 1,
-//             delay: 0.1,
-//           }}
-//         >
-//           <div className="flex py-30 px-20">
-//             <div>
-//               <img src={logo} alt="logo" className="w-45 h-45" />
-//             </div>
-//             <div>
-//               <p className="text-7xl text-white font-['Fredoka_One'] py-10 px-5">
-//                 pharmacy
-//               </p>
-//             </div>
-//           </div>
-//           <div className="flex justify-between bg-white h-14 w-150 relative bottom-25 left-25 px-5 py-1 rounded-xl">
-//             <img
-//               src={Heart}
-//               alt="heart"
-//               className="w-13 h-15 relative bottom-7"
-//             />
-
-//             <p className="text-3xl font-['Fredoka_One'] mb-2">
-//               medicine & more on minutes
-//             </p>
-//             <img src={Clock} alt="heart" className="w-12 h-12 relative top-5" />
-//           </div>
-//         </motion.div>
-//         <motion.div
-//           // animate={{}}
-//           variants={parentVariants}
-//           initial="initial"
-//           whileHover="hover"
-//           className="px-20 py-15 relative"
-//         >
-//           <motion.img
-//             variants={childVariants}
-//             src={img6}
-//             alt="icon"
-//             className="absolute z-0 h-30 top-15 right-100"
-//           />
-//           <motion.img
-//             variants={childVariants}
-//             src={img1}
-//             alt=""
-//             className="absolute h-18 right-2 z-1"
-//           />
-//           <motion.img
-//             variants={childVariants}
-//             src={img2}
-//             alt="bloodDonor"
-//             className="absolute top-52 right-65 h-30 w-70 z-1"
-//           />
-//           <motion.img
-//             variants={childVariants}
-//             src={img3}
-//             alt="location"
-//             className="absolute top-100 h-10 right-25 z-1"
-//           />
-//           <motion.img
-//             variants={childVariants}
-//             src={img4}
-//             alt="prescription"
-//             className="absolute z-1 h-30 top-103 left-5"
-//           />
-//           <motion.img
-//             variants={childVariants}
-//             src={img5}
-//             alt="map"
-//             className="absolute z-1 h-30 top-115 right-8"
-//           />
-//           <motion.img
-//             src={rectangle1}
-//             alt="mainimg"
-//             initial={{ scale: 0.5 }}
-//             animate={{ scale: 1 }}
-//             transition={{ duration: 0.5 }}
-//             className="w-90 h-120 relative"
-//           />
-//         </motion.div>
-//       </div>
-//     </div>
-//   );
-// }
 
