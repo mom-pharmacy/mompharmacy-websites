258e416881f974b7bf83b2d9c889d0104951eafa
tharunkatta10 <tkattabtech21@ced.alliance.edu.in>
2025-08-07 15:48:43 +0530

    Update EnquiryForm.jsx

--- src/components/Investors/EnquiryForm.jsx
+++ src/components/Investors/EnquiryForm.jsx
@@ -62,7 +62,7 @@ export default function EnquiryForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://18.61.201.132:3000/api/invest/add", {
+      const response = await fetch("http://mom-load-balancer10-1035236598.ap-south-1.elb.amazonaws.com:3000/api/invest/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
