be4afecf7d1cc99585489e09c718acc2cd9f2293
navyasadanapalli <navyasadanapalli@gmail.com>
2025-08-08 16:50:25 +0530

    final commit home page

--- src/components/Home/SlideBarAnimation.jsx
+++ src/components/Home/SlideBarAnimation.jsx
@@ -4,46 +4,47 @@ import careandcure from '../../assets/AboutUs/careandcure.png'
 import clock from '../../assets/AboutUs/clock.png'
 import BannerHeart from '../../assets/AboutUs/bannerheart.png'
 const sliderAnimationQuotes = [
-    {
-        id:1 , 
-        title:'Medicine is need',
-        image:careandcure
-
-    },
-    {
-        id:2 , 
-        title:'care and cure',
-        image:careandcure
-
-    },
-    {
-        id:3 , 
-        title:"medicine & more on minutes",
-        image:clock
-    },
-    {
-        id:4 , 
-        title:"mom pharmacy",
-        image:BannerHeart
-    },
-    {
-        id:5 , 
-        title:"care today cure tommorrow",
-        image:pharmacy
-    },
-    {
-        id:6 , 
-        title:"always together for better health",
-        image:BannerHeart
-    }
-]
+  {
+    id: 1,
+    title: "Medicine is need",
+    image: careandcure,
+  },
+  {
+    id: 2,
+    title: "care and cure",
+    image: careandcure,
+  },
+  {
+    id: 3,
+    title: "medicine & more on minutes",
+    image: clock,
+  },
+  {
+    id: 4,
+    title: "mom pharmacy",
+    image: BannerHeart,
+  },
+  {
+    id: 5,
+    title: "care today cure tommorrow",
+    image: pharmacy,
+  },
+  {
+    id: 6,
+    title: "always together for better health",
+    image: BannerHeart,
+  },
+  {
+    id: 7,
+    title: "Medicine is need",
+    image: pharmacy,
+  },
+];
 
 
 function SlideBarAnimation(){
 
-    // const {scrollYProgress} = useScroll()
 
-    // const x = useTransform(scrollYProgress , [0 ,1] , ["0%" , "-100%"])
 
     return <motion.div   
     animate={{ x: ["0%", "-40%"] }}
@@ -51,10 +52,10 @@ function SlideBarAnimation(){
           repeat: Infinity,
           duration: 20,
           ease: "linear",
-        }}  className="flex bg-[#dcfffb] px-2 py-5 w-fit overflow-hidden gap-6 whitespace-nowrap font-semibold text-3xl ">
+        }}  className="flex bg-[#dcfffb] px-2 animate-infinite-scroll w-fit py-5 overflow-hidden gap-6 whitespace-nowrap font-semibold text-2xl ">
         {sliderAnimationQuotes.map(item=><div key={item.id} className='flex gap-5 items-center justify-center'>
         
-            <img src={item.image} alt="img" className='h-5 w-7 ml-5 '/>
+            <img src={item.image} alt="img" className='fh-5 w-7 ml-5  '/>
           <p className="text-[#444444]" >  {item.title} </p>
             </div>)}
     </motion.div>
